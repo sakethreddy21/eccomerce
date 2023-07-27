2631a734c373b817f62b35b2bb64c6ba24d43390
sakethreddy21 <kk8429@srmist.edu.in>
2023-07-27 22:06:43 +0530

    server setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
+require("./db/connection")
 const express= require("express");
 const app = express();
 const port =8005;
-
+const mongoose = require("mongoose");
 app.listen(port, ()=>{
   console.log(`sever is running on port number ${port}`)
 })
